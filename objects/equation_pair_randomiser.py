4db04b7cf561b5c52194927f84c0803242187798
levitesuo <leevi.suotula@gmail.com>
2023-10-03 20:56:04 +0300

    Got smart equation pair generation working

--- objects/equation_pair_randomiser.py
+++ objects/equation_pair_randomiser.py
@@ -38,10 +38,39 @@ def get_smart_scramble_equation_for_sub():
         eq.manipulate_add(i, str(int(manipulators[i])*-1))
     return eq
 
+def get_good_sub_equation():
+    #a1x + b1y = c1
+    #a2x + b2y = c2
+    a1 = 1
+    b1 = 1
+    a2 = 1
+    b2 = 1
+    while a1/b1 == a2 / b2:
+        range = 9
+        solution = (r(range * -1, range), r(range * -1, range))
+        
+        y_axel1 = r(range * -1, range)
+        x_axel2 = r(range * -1, range)
+
+        c1 = y_axel1 * solution[0]
+        c2 = x_axel2 * solution[1]
+        
+        b1 = solution[0]
+        a1 = y_axel1 - solution[1]
+        
+        a2 = solution[1]
+        b2 = x_axel2 - solution[0]
+    
+    return Equation_group([f"{a1}x {b1}y = {c1}", f"{a2}x {b2}y = {c2}"])
+    
+    
+
 if __name__ == "__main__":
     eq = get_equation_pair_xy_method()
     aq = get_equation_pair_xy_method_scrambeld()
     wq = get_smart_scramble_equation_for_sub()
+    rq = get_good_sub_equation()
     print(str(eq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
     print(str(aq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
     print(str(wq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
+    print(str(rq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
