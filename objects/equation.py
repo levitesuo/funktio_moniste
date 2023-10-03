e432254a540ea0ae8b85c6a4f00d574d5ae7a337
Leevi Suotula <leevi.suotula@gmail.com>
2023-10-03 15:34:57 +0300

    Trying to make equation groups work properly

--- objects/equation.py
+++ objects/equation.py
@@ -1,14 +1,16 @@
 from function import Function
-from term import Term
 
 class Equation:
     def __init__(self, left_side, right_side = "0"):
         self.left = Function(left_side)
         self.right = Function(right_side)
-
+    
     def get_latex(self):
         return f"\({self.left.get_latex()}={self.right.get_latex()}\)"
-
+    
+    def get_string(self):
+        return f"{self.left.get_latex()}={self.right.get_latex()}"
+    
     def manipulate_add(self, termstr):
         self.right.manipulate_add(termstr)
         self.left.manipulate_add(termstr)
@@ -16,8 +18,7 @@ class Equation:
 if __name__ == "__main__":
     e = Equation("10 3x 7y")
     print(e.get_latex())
-    e.manipulate_add("+3x")
+    e.manipulate_add("3x")
     print(e.get_latex())
     e.manipulate_add("-7y")
     print(e.get_latex())
-    
--- objects/equation_group.py
+++ objects/equation_group.py
@@ -14,3 +14,11 @@ class Equation_group:
                     else: right_side += term
             if len(right_side) != 0: self.equations.append(Equation(right_side, left_side))
             else:                    self.equations.append(Equation(left_side))
+
+    def get__latex(self):
+        out_str = "\[\n\left\{\n"
+        for equation in self.equations:
+            out_str += equation.get_string()
+
+if __name__ == "__main___":
+    eg = Equation_group(["3x 4y 12", "x y 1"])
