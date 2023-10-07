93536a47f2f8929b01f4144352ab87bf23d71d95
levitesuo <leevi.suotula@gmail.com>
2023-10-07 08:35:20 +0300

    Testting pylatex

--- objects/equation_pair_randomiser.py
+++ objects/equation_pair_randomiser.py
@@ -1,6 +1,8 @@
 from equation_group import Equation_group
 #from get_random import get_nonzero_random as r
 from random import randint
+
+import numpy as np
 def r(range_start, range_end):
         out = randint(range_start, range_end)
         while out == 0: out = randint(range_start, range_end)
@@ -45,9 +47,14 @@ def get_good_sub_equation():
     b1 = 1
     a2 = 1
     b2 = 1
-    while a1/b1 == a2 / b2:
+    while b1 != 0 and b2 != 0 and a1/b1 == a2 / b2:
         range = 9
-        solution = (r(range * -1, range), r(range * -1, range))
+        s1 = r(range * -1, range)
+        s2 = r(range * -1, range)
+        while s1 == s2:
+            s1 = r(range * -1, range)
+            s2 = r(range * -1, range)
+        solution = (s1, s2)
         
         y_axel1 = r(range * -1, range)
         x_axel2 = r(range * -1, range)
@@ -60,17 +67,30 @@ def get_good_sub_equation():
         
         a2 = solution[1]
         b2 = x_axel2 - solution[0]
-    
-    return Equation_group([f"{a1}x {b1}y = {c1}", f"{a2}x {b2}y = {c2}"])
+    return [np.array([[a1, b1], [a2, b2]]), np.array([c1, c2])]
+    #return Equation_group([f"{a1}x {b1}y = {c1}", f"{a2}x {b2}y = {c2}"])
     
     
 
 if __name__ == "__main__":
-    eq = get_equation_pair_xy_method()
+    '''    eq = get_equation_pair_xy_method()
     aq = get_equation_pair_xy_method_scrambeld()
     wq = get_smart_scramble_equation_for_sub()
-    rq = get_good_sub_equation()
-    print(str(eq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
+    rq = get_good_sub_equation()'''
+    '''    print(str(eq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
     print(str(aq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
     print(str(wq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
-    print(str(rq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))
+    print(str(rq.get_latex()).replace("'", "").replace("[", "").replace("]", ""))'''
+    solutions = {}
+    for i in range(1000):
+        eq = get_good_sub_equation()
+        s = np.linalg.solve(eq[0], eq[1])
+        solution = (int(s[0]), int(s[1]))
+        if solution not in solutions: solutions[solution] = 1
+        else: solutions[solution] += 1
+    a = input()
+    for key in solutions:
+        print(key, solutions[key])
+        
+        
+    
